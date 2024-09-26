eda1c9b65ff799b893e02730f6340030eed3a2d9
Basil <basilrasheed24@gmail.com>
2024-09-26 16:20:28 +0530

    Add CORS annotation

--- backendSpringBoot/usersService/src/main/java/com/example/Users_service/controller/UsersController.java
+++ backendSpringBoot/usersService/src/main/java/com/example/Users_service/controller/UsersController.java
@@ -5,10 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
